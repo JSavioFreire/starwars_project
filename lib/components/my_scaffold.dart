034bccd716046fc61d59c1dcde74d09d415e6fe4
Sávio Freire <105501774+JSavioFreire@users.noreply.github.com>
2023-03-20 12:08:42 -0300

    Initial commit

---
+++ lib/components/my_scaffold.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyScaffold extends StatelessWidget {
+  final Widget insideScaffold;
+  const MyScaffold({super.key, required this.insideScaffold});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body: insideScaffold,
+    );
+  }
+}
---
+++ lib/services/check_is_auth.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CheckIsAuth extends StatelessWidget {
+  const CheckIsAuth({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
